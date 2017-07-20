ebe55094359726aff3dd63041eeed79dff51ebc4
Julia Simon <julia.simon@pennfoster.edu>
2017-07-20 17:12:15 -0400

    added category id column to item table

---
+++ db/migrate/20170720210848_addcategoryidtoitem.rb
@@ -0,0 +1,5 @@
+class Addcategoryidtoitem < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :items, :category, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720204512) do
+ActiveRecord::Schema.define(version: 20170720210848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170720204512) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
 end
