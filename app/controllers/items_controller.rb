d77f90fe40dcd871d037c8a93660cedf5a8deb53
Julia Simon <julia.simon@pennfoster.edu>
2017-07-21 14:08:38 -0400

    adding necessary files and code, not working currently

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @categories = Category.all
+  end
+
+  def from_category
+    @selected = Item.where(:category_id => params[:cat_id])
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, :only => :index
+  get "/fetch_items" => 'items#from_category', as: 'fetch_items'
 end
